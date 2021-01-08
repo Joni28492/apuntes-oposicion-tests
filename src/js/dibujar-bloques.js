96a0e791853e440f58eb572436abc898439ab759
Joni <joni28492@gmail.com>
2021-01-08 15:56:21 +0100

    temas separados

--- src/js/dibujar-bloques.js
+++ src/js/dibujar-bloques.js
@@ -1,62 +1,7 @@
-
+import {bloques, temasConstitucion, temasCuerposYFuerzasSeguridad, temasTrafico, temasCodigoPenal } from './temas-list';
 
 const body=document.body;
 let divContainer=document.querySelector('.container-fluid');
-
-//bloques
-const bloques = [
-    'Constitución',
-    'Cuerpos y fuerzas de seguridad', 
-    'Tráfico', 
-    'Código Penal'
-]
-const temasConstitucion = [
-    'La Constitución Española de 1978 I',
-    'La Constitución Española de 1978 II',
-    'La Constitución Española de 1978 II',
-    'Estatuto de autonomia del Principado de Asturias',
-    'Administración Local',
-    'Derecho Administrativo',
-    'Acto Administrativo',
-    'Procedimiento Administrativo',
-    'Personal al servicio de las corporaciones locales',
-    'Haciendas Loacles',
-    'Historia de Asturias',
-    'Geográfia de Asturias'];
-const temasCuerposYFuerzasSeguridad = [
-    'Ley Organica 2/86 13 Marzo, Cuerpos y Fuerzas de Seguridad I', 
-    'Ley Organica 2/86 13 Marzo, Cuerpos y Fuerzas de Seguridad II',
-    'Ley Organica 2/86 13 Marzo, Cuerpos y Fuerzas de Seguridad III',
-    'Ley Organica 2/86 13 Marzo, Cuerpos y Fuerzas de Seguridad IV',
-    'Ley Organica 4/2015 30 Marzo, Proteción de la seguridad ciudadana',
-    'Protección Civil',
-    'tema 7',
-    'tema 8',
-    'tema 9',
-    'tema 10',
-    'tema 11',
-    'tema 12'];
-const temasTrafico= [
-    'tema Trafico',
-    'tema 2',
-    'tema 3',
-    'tema 4',
-    'tema 5', 
-    'tema 6',
-    'tema 7'];
-const temasCodigoPenal = [
-    'tema Codigo Penal',
-    'tema 2',
-    'Delito de homicidio, lesiones, y contra la libertad', 
-    'Torturas, integridad moral e indemnidad sexual, libertad, derecho a la propia imagen y honor',
-    'Delitos contra el patrimonio', 
-    'Delitos contra la salud pública', 
-    'Delitos contra la seguridad vial',
-    'Delitos contra la administración Pública',
-    'Delitos contra la administracion de Justicia',
-    'Policia Local como policia judicial',
-    'Proceso Civil',
-    'Proceso contencioso Administrativo'];
 
 
 const devolverListado= ( arr ) =>{
