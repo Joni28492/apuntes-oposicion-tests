be62743661d5cb4dfd828ff32434d437be254f25
Joni <joni28492@gmail.com>
2021-02-03 18:14:13 +0100

    add teoria I

--- src/js/db/htmlteoria.db.js
+++ src/js/db/htmlteoria.db.js
@@ -11,6 +11,60 @@ const temaConstitucionI= `
         <hr>
        
     </ul>
+
+   
+   
+    <ul class="alert bg-danger text-light">
+        <h4 class="text-center m-2 col-md-12">Fechas importantes</h4>
+        <hr>
+        <li  class="col-md-3 btn alert-danger m-1">31 Octubre - Aprobación en las cortes</li>
+        <li  class="col-md-3 btn col-md-3 btn alert-danger m-1">6 Diciembre - Referendum pueblo español </li>
+        <li  class="col-md-3 btn col-md-3 btn alert-danger m-1">27 Diciembre - Promulgacion por el rey</li>
+        <li  class="col-md-3 btn col-md-3 btn alert-danger m-1">29 Diciembre - Publicación en el BOE y entrada en vigor</li>
+    </ul>
+
+    <ul class="alert bg-warning text-light">
+        <h4 class="text-start m-2"> <strong>Disposiciones</strong> 169 Articulos, 10 titulos y 1 preliminar</h4>
+        <hr>
+        <li class="col-md-3 btn alert-warning m-1">4  adicionales</li>
+        <li  class="col-md-3 btn alert-warning m-1">9  transitorias</li>
+        <li  class="col-md-3 btn alert-warning m-1">1  derogatoria</li>
+        <li class="col-md-3 btn alert-warning m-1">1  final</li>
+    </ul>
+   
+   
+   <div>
+    <h4>Caracteristicas generales</h4>
+    <h6>5 aspectos importantes</h6>
+    <p>
+        Derivada , <strong>escrita</strong>, codificada, <strong>rigida</strong>, <strong>extensa</strong>,
+        de aplicación directa e inmediata, define un estado democratico de regimen parlamentario clásico y monarquico,
+        <strong>inacabada</strong> en algunos aspectos, y en ocasiones voluntariamente ambigüa,de origen popular(pueblo),
+        Constitución <strong>consensuada</strong>
+    </p>
+
+    <p class="alert alert-warning">Ojo no confundir la rigidez con la flexibilidad</p>
+
+   </div>
+  
+
+   <div class="container alert alert-primary" >
+        <h4>Estructura y contenido</h4>
+        <hr>
+        <div>
+            <h6>Título Preliminar y Título I</h6>
+            <p>Parte dogmatica</p>
+        </div>
+
+        <div >
+            <h6>Título II al X</h6>
+            <p>Parte organica</p>
+        </div>
+
+      
+   </div>
+    
+    
 </div>`;
 
 
