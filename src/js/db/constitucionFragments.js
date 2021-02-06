e8e3668b4daba1a70e64ace2b4992fed7b5d3923
Joni <joni28492@gmail.com>
2021-02-06 13:37:43 +0100

    previo a articulos

---
+++ src/js/db/constitucionFragments.js
@@ -0,0 +1,222 @@
+const tresGrandesPoderes=`
+<ul>
+
+<hr>
+<h4>Tres Grades Poderes</h4>
+<li class="col-md-3 btn alert-success">Ejecutivo</li>
+<li class="col-md-3 btn alert-success">Legislativo</li>
+<li class="col-md-3 btn alert-success">Judicial</li>
+<hr>
+
+</ul>
+`;
+
+
+const fechasConstitucion = `
+
+<ul class="alert bg-danger text-light">
+<h4 class="text-center m-2 col-md-12">Fechas importantes</h4>
+<hr>
+<li  class="col-md-3 btn alert-danger m-1">31 Octubre - Aprobación en las cortes</li>
+<li  class="col-md-3 btn col-md-3 btn alert-danger m-1">6 Diciembre - Referendum pueblo español </li>
+<li  class="col-md-3 btn col-md-3 btn alert-danger m-1">27 Diciembre - Promulgacion por el rey</li>
+<li  class="col-md-3 btn col-md-3 btn alert-danger m-1">29 Diciembre - Publicación en el BOE y entrada en vigor</li>
+</ul>
+`;
+
+
+const disposicionesConstitucion=`
+<ul class="alert bg-warning text-light">
+<h4 class="text-start m-2"> <strong>Disposiciones</strong> 169 Articulos, 10 titulos y 1 preliminar</h4>
+<hr>
+<li class="col-md-3 btn alert-warning m-1">4  adicionales</li>
+<li  class="col-md-3 btn alert-warning m-1">9  transitorias</li>
+<li  class="col-md-3 btn alert-warning m-1">1  derogatoria</li>
+<li class="col-md-3 btn alert-warning m-1">1  final</li>
+</ul>
+
+`;
+
+const caracteristicasGeneralesConstitucion = `
+<div>
+<h4>Caracteristicas generales</h4>
+<h6>5 aspectos importantes</h6>
+<p>
+    Derivada , <strong>escrita</strong>, codificada, <strong>rigida</strong>, <strong>extensa</strong>,
+    de aplicación directa e inmediata, define un estado democratico de regimen parlamentario clásico y monarquico,
+    <strong>inacabada</strong> en algunos aspectos, y en ocasiones voluntariamente ambigüa,de origen popular(pueblo),
+    Constitución <strong>consensuada</strong>
+</p>
+
+<p class="alert alert-warning">Ojo no confundir la rigidez con la flexibilidad</p>
+
+</div>
+`;
+
+const principiosGeneralesConstitucionales =
+`
+<div class="container alert alert-dark text-start">
+<h4>Principios generales constitucionales</h4>
+<p>Estado social, democratico y de derecho</p>
+
+    <p class="text-start"><strong>Social:</strong>Promueve la igualdad</p>
+    <p class="text-start"><strong>Democratico:</strong>Doble vertiente la soberania reside en el pueblo, requiere pluralismo politico</p>
+    <p class="text-start"><strong>De derecho:</strong>La constitución establece el imperio de la ley y garantiza la supremacia del 
+    derecho sobre los poderes públicos</p>
+    <em>Iretroactividad y publicidad de la norma <small>Art. 9.3</small> </>
+
+
+</div>
+`;
+
+const estructuraYContenidoConstitucion=`
+<div class="container alert alert-primary" >
+<h4>Estructura y contenido</h4>
+<hr>
+<div class="alert-dark col-md-6 container-fluid">
+    <h3>Parte Dogmática</h3>
+
+    <div >
+        <h6> <strong>Título Preliminar</strong>(Art. 1-9) y
+         <strong>Título I</strong> - Derechos fundamentales (Art. 10-55)</h6>
+        
+    </div>
+
+    <div >
+        <h6> <strong>Título I</strong>  (Art. 10-55)</h6>
+        <p>Derechos fundamentales</p>
+        <strong>importante el capitulo II</strong>
+            <p>Sección 1ª (Art. 15-29)</p>
+            <p>Sección 2ª (ARt. 30-38)</p>
+        
+    </div>
+</div>
+<div class="container-fluid alert-dark col-md-6">
+    <h3>Parte orgánica</h3>
+
+    <div >
+        <h6><strong>Título II al X</strong></h6>
+        <p></p>
+    </div>
+
+</div>
+
+</div>
+`;
+
+
+const valoresSuperioresConstitucion = 
+`
+<ul class="alert bg-danger text-light">
+   <h4 class="text-center m-2 col-md-12">Valores Superiores estado social y democratico</h4>
+   <p>Art. 1.1</p>
+   <hr>
+   <li  class="col-md-3 btn alert-danger m-1"><strong>libertad</strong>
+    <p>
+    Autonomia del individuo, corresponde a los poderes públicos promover las condiciones
+    para que la libertad y la igualdad del individuo y de los grupos en que se integre
+    sean reales y efectivos(Art 9.2)
+    </p>
+   </li>
+   <li  class="col-md-3 btn col-md-3 btn alert-danger m-1"> <strong>justicia </strong>
+   <p>emana del pueblo y se administra en nombre del rey por sus jueces y magistrados,
+   derecho a tutela judicial efectiva</p>
+   </li>
+   <li  class="col-md-3 btn col-md-3 btn alert-danger m-1"> <strong>Igualdad</strong>
+   <p>Todos los españoles son iguales ante la ley sin discriminacion de nacimiento, raza, sexo,
+   opinion, religion o cualquier otra condicion, o circunstancia personal o social</p>
+   </li>
+   <li  class="col-md-3 btn col-md-3 btn alert-danger m-1"> <strong>Pluralismo politico</strong> Art. 6
+   <p>
+   los partidos politicos expresan el pluralismo politico concurren a la formación y manifestación de la 
+   voluntad popular y son instrumento fundamental para la participacion politica, su creación y el ejercicio 
+   de su actividad son libres dentro del respeto a la constitucion y a la ley.
+   Su estructura interna y funcionamiento deberan ser democraticos
+   </p>
+   </li>
+
+  
+</ul>
+
+`;
+
+const art139Ref=`
+<div class="alert-warning m-2">
+<h5>Art. 139</h5>
+<p>Todos los españoles tienen los mismos derechos y obligaciones dentro del territorio español</p>
+<small>No invocable</small>
+</div>
+`;
+
+
+const estadoAutonomias = `
+<ul class="alert alert-dark ">
+<h4 class="text-start m-2"> Estado de las autonomias y unidad de la nación española</h4>
+<hr>
+<p>Indisoluble unidad de la nación española, patría comun e indivisible a todos los españoles</p>
+<p>Derecho a la autonomía de las nacionalidades y regiones que la integran y solidaridad
+<small>(Art. 2)</small>entre ellas</p>
+<li class="col-md-3 btn alert-success m-1">Múnicipios</li>
+<li  class="col-md-3 btn alert-success m-1">Provincias</li>
+<li  class="col-md-3 btn alert-success m-1">Comunidades Autónomas CCAA</li>
+
+</ul>
+
+`
+
+const introDerechosFundamentales =`
+<div class="alert-info ">
+    <h2><strong>Derechos fundamentales</strong></h2>
+    <h3>Título I - Cápitulo II</h3>
+
+    <div class="container alert-primary ">
+        
+        <ul >
+
+            <hr>
+            <h4>Libertades públicas</h4>
+            <li class="col-md-3 btn alert-success">Personales</li>
+            <li class="col-md-3 btn alert-success">Civiles</li>
+            <li class="col-md-3 btn alert-success">Politicas</li>
+            <hr>
+
+        </ul>
+
+        <ul >
+
+        
+        <h4>Derechos económicos, sociales y culturales</h4>
+        <li  class="col-md-3 btn col-md-3 btn alert-warning m-1"> <strong>Económicos</strong> 
+        <p>(Art. 31,33,38,40,47,50,51)</p>
+        </li>
+
+        <li  class="col-md-3 btn col-md-3 btn alert-warning m-1"> <strong>Sociales</strong> 
+        <p>(Art. 25,28,35,37,39,41,43,49)</p>
+        </li>
+
+        <li  class="col-md-3 btn col-md-3 btn alert-warning m-1"> <strong>Politicos</strong> 
+        <p>(Art. 27,44,45,46,48)</p>
+        </li>
+
+        <p>Máximo nivel de participacion jurídica, existe una garantía añadida de reserva de ley Orgánica para el desarrollo de normativa
+        de los mismos, Por su vulneración se puede acudir a recurso de amparo ante el tribunal constitucional</p>
+        <hr>
+
+    </ul>
+    </div>
+
+</div>
+`
+
+export{
+    fechasConstitucion,
+    tresGrandesPoderes,
+    disposicionesConstitucion,
+    caracteristicasGeneralesConstitucion,
+    principiosGeneralesConstitucionales,
+    estructuraYContenidoConstitucion,
+    valoresSuperioresConstitucion,
+    art139Ref,
+    estadoAutonomias,
+    introDerechosFundamentales
+}
--- src/js/db/htmlteoria.db.js
+++ src/js/db/htmlteoria.db.js
@@ -1,102 +1,22 @@
+import { art139Ref, caracteristicasGeneralesConstitucion, disposicionesConstitucion, estadoAutonomias, estructuraYContenidoConstitucion, fechasConstitucion, introDerechosFundamentales, principiosGeneralesConstitucionales, tresGrandesPoderes, valoresSuperioresConstitucion } from "./constitucionFragments";
+
 const temaConstitucionI= `
 <div class="alert alert-info">
  <h1>Cosntitución I</h1>
-    <ul>
+ 
+   ${tresGrandesPoderes}
+   ${fechasConstitucion}
+   ${disposicionesConstitucion}
+   ${caracteristicasGeneralesConstitucion}
+   ${principiosGeneralesConstitucionales}
+   ${estructuraYContenidoConstitucion}
+   ${valoresSuperioresConstitucion}
+   ${art139Ref}
+   ${estadoAutonomias}
+   ${introDerechosFundamentales}
 
-        <hr>
-        <h4>Tres Grades Poderes</h4>
-        <li class="col-md-3 btn alert-success">Ejecutivo</li>
-        <li class="col-md-3 btn alert-success">Legislativo</li>
-        <li class="col-md-3 btn alert-success">Judicial</li>
-        <hr>
-       
-    </ul>
-
-   
-   
-    <ul class="alert bg-danger text-light">
-        <h4 class="text-center m-2 col-md-12">Fechas importantes</h4>
-        <hr>
-        <li  class="col-md-3 btn alert-danger m-1">31 Octubre - Aprobación en las cortes</li>
-        <li  class="col-md-3 btn col-md-3 btn alert-danger m-1">6 Diciembre - Referendum pueblo español </li>
-        <li  class="col-md-3 btn col-md-3 btn alert-danger m-1">27 Diciembre - Promulgacion por el rey</li>
-        <li  class="col-md-3 btn col-md-3 btn alert-danger m-1">29 Diciembre - Publicación en el BOE y entrada en vigor</li>
-    </ul>
-
-    <ul class="alert bg-warning text-light">
-        <h4 class="text-start m-2"> <strong>Disposiciones</strong> 169 Articulos, 10 titulos y 1 preliminar</h4>
-        <hr>
-        <li class="col-md-3 btn alert-warning m-1">4  adicionales</li>
-        <li  class="col-md-3 btn alert-warning m-1">9  transitorias</li>
-        <li  class="col-md-3 btn alert-warning m-1">1  derogatoria</li>
-        <li class="col-md-3 btn alert-warning m-1">1  final</li>
-    </ul>
-   
-   
-   <div>
-    <h4>Caracteristicas generales</h4>
-    <h6>5 aspectos importantes</h6>
-    <p>
-        Derivada , <strong>escrita</strong>, codificada, <strong>rigida</strong>, <strong>extensa</strong>,
-        de aplicación directa e inmediata, define un estado democratico de regimen parlamentario clásico y monarquico,
-        <strong>inacabada</strong> en algunos aspectos, y en ocasiones voluntariamente ambigüa,de origen popular(pueblo),
-        Constitución <strong>consensuada</strong>
-    </p>
-
-    <p class="alert alert-warning">Ojo no confundir la rigidez con la flexibilidad</p>
-
-   </div>
-  
-
-    <div class="container alert alert-dark text-start">
-        <h4>Principios generales constitucionales</h4>
-        <p>Estado social, democratico y de derecho</p>
-       
-            <p class="text-start"><strong>Social:</strong>Promueve la igualdad</p>
-            <p class="text-start"><strong>Democratico:</strong>Doble vertiente la soberania reside en el pueblo, requiere pluralismo politico</p>
-            <p class="text-start"><strong>De derecho:</strong>La constitución establece el imperio de la ley y garantiza la supremacia del 
-            derecho sobre los poderes públicos</p>
-            <em>Iretroactividad y publicidad de la norma <small>Art. 9.3</small> </>
-        
-    
-    </div>
-
-
-   <div class="container alert alert-primary" >
-        <h4>Estructura y contenido</h4>
-        <hr>
-        <div class="alert-dark col-md-6 container-fluid">
-            <h3>Parte Dogmática</h3>
-        
-            <div >
-                <h6> <strong>Título Preliminar</strong>(Art. 1-9) y
-                 <strong>Título I</strong> - Derechos fundamentales (Art. 10-55)</h6>
-                
-            </div>
-
-            <div >
-                <h6> <strong>Título I</strong>  (Art. 10-55)</h6>
-                <p>Derechos fundamentales</p>
-                <p>importante el capitulo II
-                    Sección 1ª (Art. 15-29)
-                    Sección 2ª (ARt. 30-38)
-                </p>
-            </div>
-        </div>
-        <div class="container-fluid alert-dark col-md-6">
-            <h3>Parte orgánica</h3>
-
-            <div >
-                <h6><strong>Título II al X</strong></h6>
-                <p></p>
-            </div>
-
-        </div>
-      
-   </div>
-    
-    
-</div>`;
+</div>
+`;
 
 
 const temaCuerposyfuerzasI= `
